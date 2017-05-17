a4e5d44f10f903fcace07ebfeba2b7d15a910432
anisak000 <anisak000@citymail.cuny.edu>
2017-05-17 19:58:00 -0400

    I wrote functions in terms of numpy to avoid loops and incorporated the suggested revisions.

--- star_distribution.py
+++ star_distribution.py
@@ -1,90 +1,39 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 # define the rotation curve
-def vel(r1):
 
-    ar1 = math.fabs(r1)
-    if (ar1 <= 5.0):
-        v1 = 1.4*ar1*math.exp(-ar1/20.0)
-    elif (ar1 > 5.0 and ar1 < 6.0):
-        max = 1.4*5*math.exp(-5.0/20.0)
+def vel(r):
+
+    ar = np.abs(r)
+    if np.any(ar <= 5.0):
+        v = 1.4*ar*np.exp(-ar/20.0)
+    elif np.any(ar > 5.0) and np.any(ar < 6.0):
+        max = 1.4*5*np.exp(-5.0/20.0)
         min = 5.0
         m = (max - min)/(-1.0)
-        v1 = m*(ar1 - 5.0) + max
+        v = m*(ar - 5.0) + max
     else:
-        v1 = 5.0
+        v = 5.0
 
-    return v1
-
-def vel(r2):
-
-    ar2 = math.fabs(r2)
-    if (ar2 <= 5.0):
-        v2 = 1.4*ar2*math.exp(-ar2/20.0)
-    elif (ar2 > 5.0 and ar2 < 6.0):
-        max = 1.4*5*math.exp(-5.0/20.0)
-        min = 5.0
-        m = (max - min)/(-1.0)
-        v2 = m*(ar2 - 5.0) + max
-    else:
-        v2 = 5.0
-
-    return v2
-
-def vel(r3):
-
-    ar3 = math.fabs(r3)
-    if (ar3 <= 5.0):
-        v3 = 1.4*ar3*math.exp(-ar3/20.0)
-    elif (ar3 > 5.0 and ar3 < 6.0):
-        max = 1.4*5*math.exp(-5.0/20.0)
-        min = 5.0
-        m = (max - min)/(-1.0)
-        v3 = m*(ar3 - 5.0) + max
-    else:
-        v3 = 5.0
-
-    return v3
+    return v
 
 
 
-def spiral():
-
-    rmin = -20
-    rmax = 20
-    npts1 = 16
-    npts2 = 8
-    npts3 = 4
+def spiral(rmin=-20, rmax=20, npts1=16, npts2=8, npts3=4, t=0.0, tmax=20.0, dt=0.04, iframe=0):
 
     r1 = np.arange(npts1, dtype=np.float64)*(rmax - rmin)/(npts1 - 1.0) + rmin
     r2 = np.arange(npts2, dtype=np.float64) * (rmax - rmin) / (npts2 - 1.0) + rmin
     r3 = np.arange(npts3, dtype=np.float64) * (rmax - rmin) / (npts3 - 1.0) + rmin
 
-    omega = np.zeros( (npts1) )
-    gamma = np.zeros( (npts2) )
-    beta = np.zeros( (npts3) )
+    v1 = np.array(vel(r1))
+    v2 = np.array(vel(r2))
+    v3 = np.array(vel(r3))
 
-    n1 = 0
-    while (n1 < npts1):
-        omega[n1] = math.fabs(vel(r1[n1])/r1[n1])
-        n1 += 1
-    n2 = 0
-    while (n2 < npts2):
-        gamma[n2] = math.fabs(vel(r2[n2])/r2[n2])
-        n2 += 1
-    n3 = 0
-    while (n3 < npts3):
-        beta[n3] = math.fabs(vel(r3[n3])/r3[n3])
-        n3 += 1
+    omega = np.absolute(v1/r1)
+    gamma = np.absolute(v2/r2)
+    beta = np.absolute(v3/r3)
 
-
-    t = 0.0
-    tmax = 20.0
-    dt = 0.04
-
-    iframe = 0
 
     while (t < tmax):
 
