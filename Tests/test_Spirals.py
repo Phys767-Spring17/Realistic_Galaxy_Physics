724564d77feef24601b385d66da7b78fe1c6f51b
anisak000 <anisak000@citymail.cuny.edu>
2017-05-19 23:52:19 -0400

    Cleaned up test code according to pep8 rules. Also relabeled Tests directory.

---
+++ Tests/test_Spirals.py
@@ -0,0 +1,23 @@
+from Spirals import vel
+import numpy as np
+import pytest
+
+max = 1.4*5*np.exp(-5.0/20.0)
+min = 5.0
+m = (max - min)/(-1.0)
+
+
+def test_spirals_1():
+    assert vel(2.0) == 1.4*2.0*np.exp(-2.0/20.0)  # this tests case r<=5
+
+
+def test_spirals_2():
+    assert vel(-2.0) == 1.4*2.0*np.exp(-2.0/20.0)  # this tests case of negative r
+
+
+def test_spirals_3():
+    assert vel(5.1) == m*(5.1 - 5.0) + max  # this tests case 5<r<6
+
+
+def test_spirals_4():
+    assert vel(7.0) == 5.0  # this tests case r>=6
